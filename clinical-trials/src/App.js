082a0cd05e2afa58def4f561e4cac7bf52797803
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2023-04-19 00:33:16 -0400

    created static table

--- clinical-trials/src/App.js
+++ clinical-trials/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar.js';
+import Results from './pages/searchresults';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      
-      <div className="footer"> <img src="./footer.png"/> </div>
+      <Results></Results>
+      <footer className="footer"> <img src="./footer.png"/> </footer>
     
     </div>
   );
--- clinical-trials/src/components/navbar/NavBar.js
+++ clinical-trials/src/components/navbar/NavBar.js
@@ -8,36 +8,38 @@ const NavBar = () => {
             </div>
             <div className="midBand">
                 <div className='clinicaltrials-logo'><img src="./clinicaltrials-logo.png"/> </div>
-                <div class="dropdown">
-                    <button>Trial Applications ▼</button>
-                    <div class="dropdown-options">
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
+                <div className='dropdownWrapper'>
+                    <div className="dropdown">
+                        <button>Trial Applications ▼</button>
+                        <div className="dropdown-options">
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                        </div>
                     </div>
-                </div>
-                <div class="dropdown">
-                    <button>About This Site ▼</button>
-                    <div class="dropdown-options">
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
+                    <div className="dropdown">
+                        <button>About This Site ▼</button>
+                        <div className="dropdown-options">
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                        </div>
                     </div>
-                </div>
-                <div class="dropdown">
-                    <button>Data About Studies ▼</button>
-                    <div class="dropdown-options">
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
+                    <div className="dropdown">
+                        <button>Data About Studies ▼</button>
+                        <div className="dropdown-options">
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                        </div>
                     </div>
-                </div>
-                <div class="dropdown">
-                    <button>Resources ▼</button>
-                    <div class="dropdown-options">
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
-                        <a href="#">xyz</a>
+                    <div className="dropdown">
+                        <button>Resources ▼</button>
+                        <div className="dropdown-options">
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                            <a href="#">xyz</a>
+                        </div>
                     </div>
                 </div>
             </div>
