d9506c1885e0ad11a247c5e21d23897afab4c122
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2023-04-23 15:07:48 -0400

    Set up trial details

--- clinical-trials/src/App.js
+++ clinical-trials/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar.js';
 import Results from './pages/searchresults';
+import TrialDetails from './pages/trialdetails';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <Results></Results>
+      <TrialDetails></TrialDetails>
       <footer className="footer"> <img src="./footer.png"/> </footer>
     
     </div>
---
+++ clinical-trials/src/pages/trialdetails.js
@@ -0,0 +1,18 @@
+import './trialdetails.css';
+import React, { Component } from 'react';
+
+class TrialDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render(){
+    return (
+        <div className="trialDetails">
+            <div> this is a place holder text</div>
+        </div>
+        )
+    }
+}
+  
+export default TrialDetails;
