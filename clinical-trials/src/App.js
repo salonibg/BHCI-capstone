2fe3a8f3df2408dc4526213eb5b7d249256c7d00
salonibg <99049157+salonibg@users.noreply.github.com>
2023-04-23 16:47:41 -0400

    moved tabs

--- clinical-trials/src/App.js
+++ clinical-trials/src/App.js
@@ -8,6 +8,7 @@ import Tabs from './pages/js/searchinputs';
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <Tabs>
         <div label="Basic Search">
           See ya later, <em>Alligator</em>!
@@ -16,10 +17,8 @@ function App() {
           After 'while, <em>Crocodile</em>!
         </div>
       </Tabs>
-      <NavBar></NavBar>
       <TrialDetails></TrialDetails>
       <footer className="footer"> <img src="./footer.png"/> </footer>
-    
     </div>
   );
 }
